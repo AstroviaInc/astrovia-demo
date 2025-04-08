f11e83d9c4f7bb4e5f59ec019dd81fb1598a4c18
AstroviaInc <Astrovia.inc@gmail.com>
2025-04-08 14:08:10 +0530

    Delete Components directory

--- Components/ui/button.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils"
-import React from "react"
-
-export const Button = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(
-  ({ className, ...props }, ref) => {
-    return <button ref={ref} className={cn("px-4 py-2 rounded-xl bg-black text-white", className)} {...props} />
-  }
-)
-Button.displayName = "Button"
--- Components/ui/card.tsx
+++
@@ -1,6 +0,0 @@
-import { cn } from "@/lib/utils"
-import React from "react"
-
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("rounded-2xl border bg-white p-4 shadow-sm", className)} {...props} />
-}
